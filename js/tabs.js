70dc4d97ea0a259f1f256ca8f9340c258b04d7a1
Allan Nogueira de Oliveira <allan.no@hotmail.com>
2015-10-27 17:43:49 -0200

    landing page animada
    landing page animada

---
+++ js/tabs.js
@@ -0,0 +1,26 @@
+jQuery(document).ready(function() {
+    jQuery('.tabs .tab-links a').on('click', function(e)  {
+        var currentAttrValue = jQuery(this).attr('href');
+ 
+        // Show/Hide Tabs
+        jQuery('.tabs ' + currentAttrValue).show().siblings().hide();
+ 
+        // Change/remove current tab to active
+        jQuery(this).parent('li').addClass('active').siblings().removeClass('active');
+ 
+        e.preventDefault();
+    });
+});
+
+// Animated Fade
+	jQuery('.tabs.animated-fade .tab-links a').on('click', function(e)  {
+		var currentAttrValue = jQuery(this).attr('href');
+
+		// Show/Hide Tabs
+		jQuery('.tabs ' + currentAttrValue).fadeIn(400).siblings().hide();
+
+		// Change/remove current tab to active
+		jQuery(this).parent('li').addClass('active').siblings().removeClass('active');
+
+		e.preventDefault();
+	});
